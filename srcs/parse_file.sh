bd28eb381ffb7f0a866722446bed16b13ed33190
Arnaud GUILLARD <arguilla@e2r8p18.42.fr>
2021-08-23 00:12:54 +0200

    remove verbose mode in write_header.sh because the output of write_header.sh wich contains the verbose text is store in the header file.

--- srcs/parse_file.sh
+++ srcs/parse_file.sh
@@ -3,7 +3,6 @@
 parse_file() {	
 	find "$dir" -maxdepth 1 -type f -name "*.c" -print0 \
 		| while IFS= read -r -d '' file; do
-			verbose "parse [$file]"
 			awk '$1 !~ "static" && /^[a-zA-Z0-9_-]+\t.*/ {
 			do {
 				current_line=$N
